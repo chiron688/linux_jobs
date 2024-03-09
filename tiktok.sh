863bf91c7755b761c19f8e73c639fc2818043cd2
chiron688 <63992772+chiron688@users.noreply.github.com>
2024-03-09 18:22:08 +0800

    Update tiktok.sh
    显示优化

--- tiktok.sh
+++ tiktok.sh
@@ -53,59 +53,53 @@ local_ipv4=$(curl $useNIC -4 -s --max-time 10 api64.ipify.org)
 local_ipv4_asterisk=$(awk -F"." '{print $1"."$2".*.*"}' <<<"${local_ipv4}")
 local_isp4=$(curl $useNIC -s -4 -A $UA_Browser --max-time 10 https://api.ip.sb/geoip/${local_ipv4} | grep organization | cut -f4 -d '"')
 
-# function MediaUnlockTest_Tiktok_Region() {
-#     echo -n -e "Tiktok Region:\t\t\c"
-#     local FtmpResult=$(curl $useNIC --user-agent "${UA_Browser}" -s --max-time 10 "https://www.tiktok.com/")
-#     local curlExitCode=$?
-
-#     if [ $curlExitCode -ne 0 ]; then
-#         echo -e "\rTiktok Region:\t\t${Font_Red}Failed (Network Connection, curl exit code: $curlExitCode)${Font_Suffix}"
-#         return
-#     fi
-
-#     local FRegion=$(echo $FtmpResult | grep '"region":' | sed 's/.*"region":"\([^"]*\)".*/\1/' | head -n 1)
-#     local FCity=$(echo $FtmpResult | grep '"geoCity":' | sed 's/.*"City":"\([^"]*\)".*/\1/' | head -n 1)
-
-#     if [ -n "$FRegion" ]; then
-#         echo -e "\rTiktok Region:\t\t${Font_Green}【${FRegion}】${Font_Suffix}"
-#     else
-#         echo -e "\rTiktok Region:\t\t${Font_Red}Failed${Font_Suffix}"
-#         return
-#     fi
-
-#     if [ -n "$FCity" ]; then
-#         echo -e "City:\t\t\t${Font_Green}【${FCity}】${Font_Suffix}"
-#     else
-#         echo -e "City:\t\t${Font_Red}Failed${Font_Suffix}"
-#         return
-#     fi
-# }
 function MediaUnlockTest_Tiktok_Region() {
-    echo -n -e " Tiktok Region:\t\t\c"
-    local Ftmpresult=$(curl $useNIC --user-agent "${UA_Browser}" -s --max-time 10 "https://www.tiktok.com/")
+    echo -n -e "Tiktok Region:\t\t\c"
+    local FtmpResult=$(curl $useNIC --user-agent "${UA_Browser}" -s --max-time 10 "https://www.tiktok.com/")
+    local curlExitCode=$?
 
-    if [[ "$Ftmpresult" = "curl"* ]]; then
-        echo -n -e "\r Tiktok Region:\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
+    if [ $curlExitCode -ne 0 ]; then
+        echo -e "\rTiktok Region:\t\t${Font_Red}Failed (Network Connection, curl exit code: $curlExitCode)${Font_Suffix}"
         return
     fi
 
     local FRegion=$(echo $Ftmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
+    local FCity=$(echo $Ftmpresult | grep '"geoCity":' | sed 's/.*"City"://' | cut -f2 -d'"' | sed 's/,.*//')
+
     if [ -n "$FRegion" ]; then
-        echo -n -e "\r Tiktok Region:\t\t${Font_Green}【${FRegion}】${Font_Suffix}\n"
+        echo -e "\rTiktok Region:\t\t${Font_Green}【${FRegion}】${Font_Suffix}\n"
+    else
+        echo -e "\rTiktok Region:\t\t${Font_Red}Failed${Font_Suffix}\n"
         return
     fi
 
+    if [ -n "$FCity" ]; then
+        echo -e "City:\t\t\t${Font_Green}【${FCity}】${Font_Suffix}\n"
+    else
+        echo -e "City:\t\t${Font_Red}Failed${Font_Suffix}\n"
+        return
+    fi
+    
     local STmpresult=$(curl $useNIC --user-agent "${UA_Browser}" -sL --max-time 10 -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-Encoding: gzip" -H "Accept-Language: en" "https://www.tiktok.com" | gunzip 2>/dev/null)
     local SRegion=$(echo $STmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
+    local SCity=$(echo $STmpresult | grep '"geoCity":' | sed 's/.*"City"://' | cut -f2 -d'"' | sed 's/,.*//')
+
     if [ -n "$SRegion" ]; then
-        echo -n -e "\r Tiktok Region:\t\t${Font_Yellow}【${SRegion}】(可能为IDC IP)${Font_Suffix}\n"
-        return
+        echo -e "\rTiktok Region:\t\t${Font_Green}【${FRegion}】${Font_Suffix}\n"
     else
-        echo -n -e "\r Tiktok Region:\t\t${Font_Red}Failed${Font_Suffix}\n"
+        echo -e "\rTiktok Region:\t\t${Font_Red}Failed${Font_Suffix}\n"
         return
     fi
 
+    if [ -n "$SCity" ]; then
+        echo -e "City:\t\t\t${Font_Green}【${FCity}】${Font_Suffix}"
+        echo -e "\r可能为IDC IP\n"
+    else
+        echo -e "City:\t\t${Font_Red}Failed${Font_Suffix}"
+        return
+    fi
 }
+
 
 
 function Heading() {
